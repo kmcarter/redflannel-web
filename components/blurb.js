c9125654eed49d13ede74fab918f1a9561dd6cdd
Kelly Carter <kelly@redflannel.co>
2020-09-17 18:19:50 -0700

    Rearrange services, blurb styling, added ugly border

--- components/blurb.js
+++ components/blurb.js
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Blurb = props => {
   return (
-    <Grid item xs={12} md={6}>
+    <Box border={props.special ? 1 : 0} borderColor="primary.main">
       <Box className={"icon major style" + (props.idx % 5 + 1)} mx="auto" mb={3} width="12rem" height="12rem" py="3rem">
         <FontAwesomeIcon icon={props.icon} size="6x" fixedWidth />
       </Box>
       <Typography component="h3" variant="h4" gutterBottom>{props.name}</Typography>
       {props.children}
-    </Grid>
+    </Box>
   );
 }
 
--- pages/_services.js
+++ pages/_services.js
@@ -1,5 +1,5 @@
 import { Grid, Typography } from "@material-ui/core";
-import { faCode, faLaptopCode, faUsers, faComments } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faLaptopCode, faUsers, faComments, faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 
 import Section from "../components/section";
 import SectionHeader from "../components/section-header";
@@ -7,27 +7,35 @@ import Blurb from '../components/blurb';
 
 export default function Services() {
   return (
-  <Section special>
-    <SectionHeader>Services</SectionHeader>
-    <Grid container spacing={8} justify="space-between">
-      <Grid container spacing={8} item xs={12} md={6} justify="space-between" className="features">
-        <Blurb idx={1} icon={faCode} name="Custom development">
-          <Typography paragraph>From API integrations to custom themes, I can whatever whatever</Typography>
-        </Blurb>
-        <Blurb idx={2} icon={faUsers} name="Team player">
-          <Typography paragraph>If you need an extra set of hands for a project, I have part-time availability.</Typography>
-        </Blurb>
-        <Blurb idx={3} icon={faLaptopCode} name="Website build">
-          <Typography paragraph>Whether you need a refresh or are jumpstarting a new brand, I can build custom or templated websites.</Typography>
-        </Blurb>
-        <Blurb idx={4} icon={faComments} name="Consulting">
-          <Typography paragraph>Troubleshooting, research, and answering that basic, perennial question: 'is it possible to...?'</Typography>
-        </Blurb>
+    <Section special>
+      <SectionHeader>Services</SectionHeader>
+      <Grid container spacing={8} justify="space-between">
+        <Grid item xs={12} md={4}>
+          <Blurb idx={1} icon={faCode} name="Custom development">
+            <Typography paragraph>From API integrations to custom themes, I can whatever whatever</Typography>
+          </Blurb>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Blurb idx={3} icon={faLaptopCode} name="Website build">
+            <Typography paragraph>Whether you need a refresh or are jumpstarting a new brand, I can build custom or templated websites.</Typography>
+          </Blurb>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Blurb idx={4} icon={faComments} name="Consulting">
+            <Typography paragraph>Troubleshooting, research, and answering that basic, perennial question: 'is it possible to...?'</Typography>
+          </Blurb>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Blurb idx={2} icon={faUsers} name="Team player">
+            <Typography paragraph>If you need an extra set of hands for a project, I have part-time availability.</Typography>
+          </Blurb>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Blurb special idx={4} icon={faExclamationTriangle} name="Note!">
+            <Typography paragraph>I can be a short-term, less than 20 hours/week contract hire or work as a consultant on your project. I'm happy to onboard with the rest of your team and lend a hand or work alone. Billing practices are flexible: by the hour or by the project. <strong>Please note that I currently only have part time availability.</strong></Typography>
+          </Blurb>
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Typography paragraph>I can be a short-term, less than 20 hours/week contract hire or work as a consultant on your project. I'm happy to onboard with the rest of your team and lend a hand or work alone. Billing practices are flexible: by the hour or by the project. <strong>Please note that I currently only have part time availability.</strong></Typography>
-      </Grid>
-    </Grid>
-  </Section>
+    </Section>
   );
 }
