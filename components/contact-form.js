eee3dafc000c677ecae16283b9d1e11c9fcccdb9
Kelly Carter <kelly@redflannel.co>
2020-09-20 18:59:00 -0700

    Moved SES and recaptcha verification to server side

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,117 +1,102 @@
-import { Grid, Input, TextField, Box, Button, Link, Snackbar } from '@material-ui/core';
+import { useReducer } from 'react';
+import { Grid, TextField, Box, Button, Link, Snackbar } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane, faProjectDiagram } from "@fortawesome/free-solid-svg-icons";
-import AWS from 'aws-sdk';
-import { GoogleReCaptcha } from "react-google-recaptcha-v3";
+import { faPaperPlane, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
-export default class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+export default function ContactForm(props) {
+  const { executeRecaptcha } = useGoogleReCaptcha();
+  const [state, setState] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {
       name: "",
       email: "",
       projDesc: "",
       token: "",
+      open: false,
       success: false,
-      message: null
-    };
+      message: null,
+      sending: false
+    }
+  );
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-
-    AWS.config.update({
-      apiVersion: '2010-12-01',
-      region: 'us-west-2',
-      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
-    });
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = async (e) => {
+    setState({sending: true});
     e.preventDefault();
 
-    if (this.state.token === "") {
+    const token = await executeRecaptcha("contact");
+    if (token === "") {
       return;
     }
 
-    let emailContent = `<html><body><dl><dt>Name</dt><dd>${this.state.name}</dd><dt>Email</dt><dd>${this.state.email}</dd><dt>Project description</dt><dd>${this.state.projDesc}</dd></dl></body></html>`;
-    var params = {
-      Destination: { /* required */
-        ToAddresses: [ 'kelly@redflannel.co' ]
+    fetch("/api/contact", {
+      method: "post",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
       },
-      Message: { /* required */
-        Body: { /* required */
-          Html: {
-           Charset: "UTF-8",
-           Data: emailContent
-          },
-          Text: {
-           Charset: "UTF-8",
-           Data: emailContent
-          }
-         },
-         Subject: {
-          Charset: 'UTF-8',
-          Data: 'Test email'
-         }
-        },
-      Source: "admin@redflannel.co", /* required */
-      ReplyToAddresses: [ this.state.email ],
-    };
-
-    // Create the promise and SES service object
-    var sendPromise = new AWS.SES().sendEmail(params).promise();
-    const thisComp = this;
-
-    // Handle promise's fulfilled/rejected states
-    sendPromise.then((data) => {
-        console.log("Email sent. Message ID:", data.MessageId);
-        thisComp.setState({open: true, success: true, message: "Thanks for reaching out! You'll hear from me soon.", name: "", email: "", projDesc: ""});
-      }).catch((err) => {
-        console.error(err, err.stack);
-        thisComp.setState({open: true, success: false, message: <>Your message could not be sent, but an error has been logged. <Link href="mailto:kelly@redflannel.co?subject=Your contact form errored" target="_blank">Send me an email</Link> instead.</>});
-      });
+      body: JSON.stringify({
+        name: state.name,
+        email: state.email,
+        projDesc: state.projDesc,
+        token: token
+      })
+    }).then((res) => {
+      return res.json();
+    }).then(content => {
+      if (content.success) {
+        console.log("Email sent. Message ID:", content.sesMessageId);
+        setState({sending: false, open: true, success: true, message: "Thanks for reaching out! You'll hear from me soon.", name: "", email: "", projDesc: ""});
+      } else {
+        throw new Error(content.message);
+      }
+    }).catch(error => {
+      setState({sending: false, open: true, success: false, message: "Your message could not be sent, but an error has been logged."});
+      console.error(error);
+    });
   }
 
-  handleClose(event, reason) {
+  const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
 
-    this.setState({open: false});
+    setState({open: false});
   };
 
-  render() {
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6}>
-              <TextField required id="name" name="name" label="Name" value={this.state.name} fullWidth
-                autoComplete="name" variant="outlined" color="secondary" onChange={(e) => this.setState({name: e.target.value})} />
-              <TextField required id="email" name="email" label="Email address" value={this.state.email} fullWidth
-                autoComplete="email" variant="outlined" color="secondary" onChange={(e) => this.setState({email: e.target.value})}
+              <TextField required id="name" name="name" label="Name" value={state.name} fullWidth
+                autoComplete="name" variant="outlined" color="secondary" onChange={(e) => setState({name: e.target.value})} />
+              <TextField required id="email" name="email" label="Email address" value={state.email} fullWidth
+                autoComplete="email" variant="outlined" color="secondary" onChange={(e) => setState({email: e.target.value})}
                 margin="normal" type="email" />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField required id="projdesc" name="projdesc" label="Describe your project" value={this.state.projDesc} fullWidth
-                autoComplete="off" variant="outlined" color="secondary" onChange={(e) => this.setState({projDesc: e.target.value})}
+              <TextField required id="projdesc" name="projdesc" label="Describe your project" value={state.projDesc} fullWidth
+                autoComplete="off" variant="outlined" color="secondary" onChange={(e) => setState({projDesc: e.target.value})}
                 multiline rows={4} />
             </Grid>
             <Grid item component={Box} xs={12} textAlign="right">
-              <GoogleReCaptcha onVerify={(token) => this.setState({token})} />
-              <Input type="hidden" name="token" value={this.state.token} />
-              <Button variant="contained" color="primary" type="submit" size="large" endIcon={<FontAwesomeIcon icon={faPaperPlane} />}>Send</Button>
+              <Button variant="contained" color="primary" type="submit" size="large" disabled={state.sending}
+                endIcon={<FontAwesomeIcon icon={state.sending ? faSyncAlt : faPaperPlane} />} spin={state.sending.toString()}>
+                  {state.sending ? "Sending" : "Send"}
+              </Button>
             </Grid>
           </Grid>
         </form>
-        <Snackbar anchorOrigin={{vertical: "top", horizontal: "center"}} open={this.state.open} autoHideDuration={4000} onClose={this.handleClose}>
-          <MuiAlert elevation={6} variant="filled" severity={this.state.success ? "success" : "error"}>
-            {this.state.message}
+        <Snackbar anchorOrigin={{vertical: "top", horizontal: "center"}} open={state.open} autoHideDuration={4000} onClose={handleClose}>
+          <MuiAlert elevation={6} variant="filled" severity={state.success ? "success" : "error"}>
+            {state.message}
+            {!state.success &&
+              <><Link href="mailto:kelly@redflannel.co?subject=Your contact form errored" target="_blank" color="textSecondary">Send me an email</Link>{" "}instead.</>
+            }
           </MuiAlert>
         </Snackbar>
       </>
     );
-  }
 }
