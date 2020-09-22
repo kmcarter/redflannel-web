bab82e33d513e4c21b27971b0c3f67d21243d932
Kelly Carter <kelly@redflannel.co>
2020-09-21 20:12:07 -0700

    Header and body styling changes

--- components/header.js
+++ components/header.js
@@ -1,19 +1,26 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, useMediaQuery } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
 
 import Link from "./link";
 
 export default function Header(props) {
-  const pt = props.alt ? 15 : 20;
-  const pb = props.alt ? 10 : 15;
+  // TODO: This is hokey, but will do for now.
+  const theme = useTheme();
+  const isLgOrUp = useMediaQuery(theme.breakpoints.up('lg'));
+  const isMedOrUp = useMediaQuery(theme.breakpoints.up('md'));
+  const isXSmallOrDown = useMediaQuery(theme.breakpoints.down('xs'));
+  const pt = props.alt ? 15 : (isMedOrUp ? 20 : 10);
+  const pb = props.alt ? 10 : (isMedOrUp ? 15 : 10);
+  const px = isLgOrUp ? 15 : (isXSmallOrDown ? 1 : 10);
 
   return (
-    <Box id="header" component="header" color="primary.contrastText" className={"alt" + (props.alt ? " interior" : "")} px={15} pt={pt} pb={pb} textAlign="center">
+    <Box id="header" component="header" color="primary.contrastText" className={"alt" + (props.alt ? " interior" : "")} px={px} pt={pt} pb={pb} textAlign="center">
       {!props.alt &&
         <>
           <Typography component="h1" variant="h1" color="textSecondary">
-            <Box className="logo">
+            <Box width={isMedOrUp ? "60%" : "100%"} className="logo image">
               <img src="/RF_logo_master-01.png" alt={props.h1} />
             </Box>
           </Typography>
